bfd8c896a40331fa30a1ef162f62e295a08142c7
unknown <ankit@lybtechnology.com>
2026-07-17 17:42:02 +0530

    header link

--- src/components/ui/UserDetails.tsx
+++ src/components/ui/UserDetails.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaPhoneAlt, FaInstagram, FaTwitter } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { FiFacebook, FiLinkedin } from "react-icons/fi";
+import { FaXTwitter } from "react-icons/fa6";
 
 import styles from "@/styles/components/ui/UserDetails.module.css";
 
@@ -15,8 +16,8 @@ function UserDetails() {
         </div>
         {/* <div className={styles.line}></div> */}
         <div className={styles.detailItem}>
-          {/* <MdEmail /> */}
-          {/* <h4 className={styles.detailsText}>support@zanifestinsurance.com </h4> */}
+          <MdEmail />
+          <h4 className={styles.detailsText}>support@zanifestinsurance.com </h4>
         </div>
         {/* <button onClick={() => signOut()}>
           Logout
@@ -28,16 +29,16 @@ function UserDetails() {
         </div> */}
       </div>
       <div className={styles.logosCont}>
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/zanifestinsurance?igsh=MXgybG9tNWJ3Yzhhbg%3D%3D" target="_blank" rel="noopener noreferrer">
         <FaInstagram className={styles.socialIcon}  />
       </a>
       <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-        <FaTwitter  />
-      </a>
-      <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+  <FaXTwitter />
+</a>
+      <a href="https://www.facebook.com/people/Zanifest-Insurance/61587951775182/?rdid=ugTU8cAvrghxwvyw&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F18a9DHo2th%2F" target="_blank" rel="noopener noreferrer">
         <FiFacebook  />
       </a>
-      <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/in/mandeep-rathee-a5665010?utm_source=share_via&utm_content=profile&utm_medium=member_android" target="_blank" rel="noopener noreferrer">
         <FiLinkedin  />
       </a>
       </div>
